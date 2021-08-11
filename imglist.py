beb47d89ff6836c255877a8051d4b4af3bb46eca
Brian Kleiner <brian.kleiner@fcb.com>
2021-08-11 15:21:39 -0400

    adding more images because why not

--- imglist.py
+++ imglist.py
@@ -1,4 +1,5 @@
 images = {
     "./images/smile.jpg": "https://m.media-amazon.com/images/I/51zLZbEVSTL._AC_SL1200_.jpg",
-    "./images/frown.png": "http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/frowning-face.png"
+    "./images/frown.png": "http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/frowning-face.png",
+    "./images/apple.png" : "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/128px-Apple_logo_black.svg.png"
 }
