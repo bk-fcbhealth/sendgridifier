82f0400812e0ea87ca4076960bdfb1ff4b36cab2
Brian Kleiner <brian.kleiner@fcb.com>
2021-08-11 12:09:39 -0400

    duplicating index.html

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
+# Importing the image names from the image list
 import imglist
+# Duplicating folders
+import shutil
 
 # Create new HTML file
-open("createdfile.html", "w")
+# open("createdfile.html", "w")
+
+
+# Duplicate index.html
+shutil.copy('index.html', "sendgrid-index.html")
+
 
 # Loop through Image List to get all image locations
 imgNames = imglist.images.keys()
