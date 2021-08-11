2a6ff808c457ae9180538657d287a04c89c5ff84
Brian Kleiner <brian.kleiner@fcb.com>
2021-08-10 23:10:47 -0400

    parsing all image locations

--- test.py
+++ test.py
@@ -3,5 +3,9 @@ import imglist
 # Create new HTML file
 open("createdfile.html", "w")
 
-# Loop through Image List
-print(imglist.images)
+# Loop through Image List to get all image locations
+imgNames = imglist.images.keys()
+for x in imgNames:
+    print(x)
+
+
