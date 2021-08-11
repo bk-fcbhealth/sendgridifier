17db354e8bde180127274b1c1804553168865ec5
Brian Kleiner <brian.kleiner@fcb.com>
2021-08-11 12:20:30 -0400

    text is replacing with dummy text

--- test.py
+++ test.py
@@ -11,9 +11,22 @@ import shutil
 shutil.copy('index.html', "sendgrid-index.html")
 
 
+#input file
+originalFile = open("index.html", "rt")
+#output file to write the result to
+sendgridFile = open("sendgrid-index.html", "wt")
+
+
 # Loop through Image List to get all image locations
 imgNames = imglist.images.keys()
 for x in imgNames:
     print(x)
+    #for each line in the input file
+    for line in originalFile:
+        #read replace the string and write to output file
+        sendgridFile.write(line.replace(x, "here is the replacement text"))
 
 
+# close input and output files
+originalFile.close()
+sendgridFile.close()
